4c23d2f4aeb9544d4178b22e4277482d4196b784
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2021-12-31 16:21:18 -0300

    Criando a tela para cadastrar os Departamentos

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -36,7 +36,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartamentoAction() {
-		System.out.println("Cadastrou departamento");
+		carregarTela("/gui/ListaDepartamento.fxml");
 	}
 
 	@FXML
